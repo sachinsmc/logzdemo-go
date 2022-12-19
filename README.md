20bbee2f2d041efe18947c422be3c8ecd3acd4e4
Sachin Chavan <chavansachin295@gmail.com>
2022-12-19 21:15:28 +0400

    add demo code

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # logzdemo-go
 Golang logz.io demo
+
+## Setup & Installation
+
+Create account on logz and get Account Token key update key in config account_token.
+
+Donwload and run the OpenTelemetry Collector 
+
+[Intel OSX](https://github.com/open-telemetry/opentelemetry-collector-contrib/releases/download/v0.33.0/otelcontribcol_darwin_amd64)
+
+[Silicon OSX](https://github.com/open-telemetry/opentelemetry-collector-contrib/releases/download/v0.33.0/otelcontribcol_darwin_arm64)
+
+```bash
+./otelcontribcol_darwin_amd64 --config config.yaml
+```
+
+Run go webserver
+
+```bash
+go run *.go
+```
+
+[Check logz tracing](https://app.logz.io/#/dashboard/kibana)
+
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func initProvider() func() {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("test-service"),
+		),
+	)
+	handleErr(err, "failed to create resource")
+
+	traceExporter, err := otlptracehttp.New(ctx,
+		otlptracehttp.WithInsecure(),
+		otlptracehttp.WithEndpoint("localhost:4318"),
+	)
+	handleErr(err, "failed to create trace exporter")
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	return func() {
+		handleErr(tracerProvider.Shutdown(ctx), "failed to shutdown TracerProvider")
+	}
+}
+
+func main() {
+	shutdown := initProvider()
+	defer shutdown()
+
+	uk := attribute.Key("username")
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		span := trace.SpanFromContext(ctx)
+		bag := baggage.FromContext(ctx)
+		span.AddEvent("handling this...", trace.WithAttributes(uk.String(bag.Member("username").Value())))
+
+		_, _ = io.WriteString(w, "Hello, world!\n")
+	}
+
+	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
+
+	http.Handle("/hello", otelHandler)
+	err := http.ListenAndServe(":7777", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleErr(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s: %v", message, err)
+	}
+}
